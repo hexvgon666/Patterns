2d1e101f758d7c6637c6803a427b2618a435a34b
hexvgon666 <qualiaquok@yandex.ru>
2024-09-30 13:22:21 +0300

    check

--- src/Students.kt
+++ src/Students.kt
@@ -1,80 +1,47 @@
 class Student(
-    private var id: Int,
-    private var lastName: String,
-    private var firstName: String,
-    private var patronymic: String,
-    private var phone: String? = null,
-    private var telegram: String? = null,
-    private var email: String? = null,
-    private var git: String? = null
+    val id: Int,
+    val lastName: String,
+    val firstName: String,
+    val patronymic: String,
+    var phone: String? = null,
+    var telegram: String? = null,
+    var email: String? = null,
+    var git: String? = null
 ) {
-    // Геттеры и сеттеры
-    class Student(
+    constructor(
+        id: Int,
+        lastName: String,
+        firstName: String,
+        patronymic: String
+    ) : this(id, lastName, firstName, patronymic, null, null, null, null)
+
+    constructor(
         id: Int,
         lastName: String,
         firstName: String,
         patronymic: String,
-        phone: String? = null,
-        telegram: String? = null,
-        email: String? = null,
-        git: String? = null
-    ) {
-        private var _id: Int = id
-        var id: Int
-            get() = _id
-            set(value) {
-                _id = value
-            }
+        phone: String
+    ) : this(id, lastName, firstName, patronymic, phone, null, null, null)
 
-        private var _lastName: String = lastName
-        var lastName: String
-            get() = _lastName
-            set(value) {
-                _lastName = value
-            }
+    constructor(
+        id: Int,
+        lastName: String,
+        firstName: String,
+        patronymic: String,
+        phone: String,
+        telegram: String
+    ) : this(id, lastName, firstName, patronymic, phone, telegram, null, null)
 
-        private var _firstName: String = firstName
-        var firstName: String
-            get() = _firstName
-            set(value) {
-                _firstName = value
-            }
+    constructor(
+        id: Int,
+        lastName: String,
+        firstName: String,
+        patronymic: String,
+        phone: String,
+        telegram: String,
+        email: String
+    ) : this(id, lastName, firstName, patronymic, phone, telegram, email, null)
 
-        private var _patronymic: String = patronymic
-        var patronymic: String
-            get() = _patronymic
-            set(value) {
-                _patronymic = value
-            }
-
-        private var _phone: String? = phone
-        var phone: String?
-            get() = _phone
-            set(value) {
-                _phone = value
-            }
-
-        private var _telegram: String? = telegram
-        var telegram: String?
-            get() = _telegram
-            set(value) {
-                _telegram = value
-            }
-
-        private var _email: String? = email
-        var email: String?
-            get() = _email
-            set(value) {
-                _email = value
-            }
-
-        private var _git: String? = git
-        var git: String?
-            get() = _git
-            set(value) {
-                _git = value
-            }
-    }
     override fun toString(): String {
         return "Student(id=$id, fullName='$lastName $firstName $patronymic', phone=$phone, telegram=$telegram, email=$email, git=$git)"
     }
