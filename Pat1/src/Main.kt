3412bdf365f2401485370a20d4beba369ebf87c3
Мищенко Н. <147876227+hexvgon666@users.noreply.github.com>
2024-11-05 17:59:13 +0300

    Выполнено лр1

--- Pat1/src/Main.kt
+++ Pat1/src/Main.kt
@@ -1,14 +1,78 @@
 fun main() {
-    val student1 = Student.Builder(1, "Иванов", "Иван", "Иванович")
-        .phone("123456789")
-        .email("ivan@mail.com")
-        .build()
+    val example_1 = Student(
+         1,
+        "Фолгимштейн",
+        "Федор",
+        "Крузинштейн",
+        "+7-989-345-34-67",
+        "@ROOMdoom",
+        "RoomDoom@mail.ru",
+        "https://github.com/Fedor"
+    )
 
-    val student2 = Student.Builder(2, "Петров", "Петр", "Петрович")
-        .telegram("@petr")
-        .git("github.com/petr")
-        .build()
+    val example_2 = Student(
+        id = 2,
+        surname = "Шаповалова",
+        name = "Карина",
+        phone = "+7-656-134-67-89"
+    )
 
-    println(student1)
-    println(student2)
-}
+    val example_3 = Student(
+        id = 3,
+        surname = "Зайцев",
+        name = "Иехонтий",
+        email = "zoom@example.com"
+    )
+
+    val example_4 = Student(
+        id = 4,
+        surname = "Смурыгин",
+        name = "Яхонт",
+        telegram = "@yakhont"
+    )
+
+
+    val example_hashMap = hashMapOf<String, Any?>(
+        "id" to 5,
+        "surname" to "Фуркин",
+        "name" to "Иван",
+        "patronymic" to "Степанович",
+        "phone" to "+7-987-336-56-89",
+        "telegram" to "@samsam",
+        "mail" to "ivan@mail.ru"
+    )
+
+    val example_validPhone = hashMapOf<String, Any?>(
+        "id" to 6,
+        "surname" to "Кирсанов",
+        "name" to "Сергей",
+        "patronymic" to "Генадьевич",
+        "phone" to "+7-916-123-45-67", // Корректный номер
+        "telegram" to "ivan_ivanov",
+        "mail" to "ivan@example.com",
+        "git" to "ivan_git"
+    )
+    val validPhone = Student(example_validPhone)
+
+    val valid = Student(
+        id = 4,
+        surname = "Смурыгин",
+        name = "Яхонт",
+        git = "https://github.com/Smyrk"
+    )
+
+    valid.setContacts(phone = "+7-916-123-45-67", telegram = "johndoe", mail = "sdfasf@mail.ru")
+
+    // Проверяем валидность данных после установки контактов
+    val isValid = valid.validate()
+    println("Данные студента валидны: $isValid")
+
+
+    println(example_1)
+    println(example_2)
+    println(example_3)
+    println(example_4)
+
+    val hash = Student(example_hashMap)
+    println(hash)
+    println(validPhone)
