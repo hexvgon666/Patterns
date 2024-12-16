ceff1326c724ec02710888d66dc37f7c22b072ca
Мищенко Н. <147876227+hexvgon666@users.noreply.github.com>
2024-12-16 18:00:28 +0300

    add methods in Data_list select, get_selected

--- Pat1/src/Data_list.kt
+++ Pat1/src/Data_list.kt
@@ -1,2 +1,31 @@
 class Data_list(val data: Array<Any>) {
+    private val select = mutableListOf<Int>()
+    fun select(num:Int)
+    {
+        if(num < data.size)
+        {
+            select.add(num)
+        }
+    }
+    fun get_selected() : MutableList<Int>
+    {
+        return select
+    }
+    fun get_names(id:Int): Array<String>
+    {
+        var name = data[id]!!::class.java.declaredFields.map { it.name }.toTypedArray()
+        name[0] = id.toString()
+        return name
+    }
+    fun get_data(): Data_table
+    {
+        var attr = mutableListOf<List<Any>>()
+        for (id in select)
+        {
+            var arg = mutableListOf<Any>(id)
+            arg.add(data[id]!!::class.java.declaredFields.map { it }.toList())
+            attr.add(arg)
+        }
+        return Data_table(attr)
+    }
 }
